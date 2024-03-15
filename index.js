ca45f0389bb6b218e1ad609205bfb1031d3ea2af
CarlHockey <thebicycarl@gmail.com>
2024-03-16 10:56:22 +1100

    initialize google ai

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+require('dotenv').config()
+
 // Import and initialise Google AI
 
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 // Fetch API key
+
+const API_KEY = process.env.GOOGLE_AI_API_KEY
+// const genAI = new GoogleGenerativeAI(API_KEY);
+
+console.log(API_KEY)
