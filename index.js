9e9f6b88fed57d13fb3bdb2f9a03a67bcf878dcb
CarlHockey <thebicycarl@gmail.com>
2024-03-16 19:39:32 +1100

    add conversation history

--- index.js
+++ index.js
@@ -15,13 +15,14 @@ const API_KEY = process.env.GOOGLE_AI_API_KEY
 // create new instance of the Google AI
 const genAI = new GoogleGenerativeAI(API_KEY);
 
-// create a new model
+// array to store conversation history
+let conversation_history = []
 
 async function run() {
     // For text-only input, use the gemini-pro model
     const model = genAI.getGenerativeModel({ model: "gemini-pro"});
   
-    const prompt = "Write me a haiku about my beautiful girlfriend, Nadia"
+    const prompt = await getUserInput("Hello, how can I help?")
   
     const result = await model.generateContent(prompt);
     const response = await result.response;
@@ -29,4 +30,10 @@ async function run() {
     console.log(text);
   }
   
+  
+  function getUserInput(message) {
+    let testInput = prompt(message)
+    return testInput
+  }
+
   run();
