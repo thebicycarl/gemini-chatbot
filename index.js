527be88d34e5f0f5ea5eb4bfa94b3a6edcfbb65c
CarlHockey <thebicycarl@gmail.com>
2024-03-23 15:48:40 +1100

    update

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // setup the .env file
 
 import dotenv from 'dotenv'
+dotenv.config()
+
+// install promptSync to get user input in terminal 
 import promptSync from 'prompt-sync'
 const promptUser = promptSync()
 
-dotenv.config()
 
 // Import Google AI package
 
@@ -14,26 +16,19 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const API_KEY = process.env.GOOGLE_AI_API_KEY
 
-// create new instance of the Google AI
+// Create new instance of the Google AI
 const genAI = new GoogleGenerativeAI(API_KEY);
 
-// array to store conversation history
+// Array to store conversation history
 
 async function run() {
-  // For text-only input, use the gemini-pro model
   const model = genAI.getGenerativeModel({ model: "gemini-pro" })
 
+  let convo_history = [
+  ]
+
   const chat = model.startChat({
-    history: [
-      {
-        role: "user",
-        parts: [{ text: "Hello, I have 2 dogs in my house." }],
-      },
-      {
-        role: "model",
-        parts: [{ text: "Great to meet you. What would you like to know?" }],
-      },
-    ],
+    history: convo_history,
     generationConfig: {
       maxOutputTokens: 100,
     },
