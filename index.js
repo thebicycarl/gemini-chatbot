b233b2fd9c74857109064be1eb0ef5f394feb884
CarlHockey <thebicycarl@gmail.com>
2024-03-23 14:55:03 +1100

    promp working with multi-chat format

--- index.js
+++ index.js
@@ -19,24 +19,39 @@ const genAI = new GoogleGenerativeAI(API_KEY);
 
 // array to store conversation history
 
-let conversation_history = []
-
 async function run() {
-    // For text-only input, use the gemini-pro model
-    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-  
-    const prompt = await getUserInput()
-  
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
-    console.log(text);
-  }
-  
-  
-  function getUserInput() {
-    let testInput = promptUser('What would you like to know: ')
-    return testInput
-  }
+  // For text-only input, use the gemini-pro model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" })
 
-  run();
+  const chat = model.startChat({
+    history: [
+      {
+        role: "user",
+        parts: [{ text: "Hello, I have 2 dogs in my house." }],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Great to meet you. What would you like to know?" }],
+      },
+    ],
+    generationConfig: {
+      maxOutputTokens: 100,
+    },
+  })
+
+  const msg = getUserInput()
+
+  const result = await chat.sendMessage(msg)
+  const response = await result.response
+  const text = response.text()
+  console.log(text)
+
+}
+
+run()
+
+
+function getUserInput() {
+  let user_input = promptUser('What would you like to know: ')
+  return user_input
+}
